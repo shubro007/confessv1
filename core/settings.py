6c6147b11a2ed4a45a1042466a62c25c8724f8a7
shubro07 <136472052+shubro007@users.noreply.github.com>
2024-07-09 02:23:03 +0530

    update

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
 ALLOWED_HOSTS = ['192.168.29.230','127.0.0.1','192.168.173.25']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
